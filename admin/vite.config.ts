ffc660a4b8284a504c84d8f7226bc785a91925d5
yaywiin <yaywiin@github.com>
2026-08-26 12:20:28 -0600

    fix(admin): set base to './' so admin works on dedicated subdomain admin.sansahbikes.com

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/admin/',
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
